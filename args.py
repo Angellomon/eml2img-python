285d884995af56f4a65e29e3b1955a612383ff65
angellomon <angelmtzdiaz@gmail.com>
2025-09-01 16:20:00 -0600

    fix: inputs
    Better arg parsing

--- args.py
+++ args.py
@@ -8,9 +8,10 @@ class ArgParser:
 
     def __init__(self) -> None:
         self.parser = argparse.ArgumentParser(prog=PROGRAM_NAME)
-        self._setup_args()
+        self._setup_inputs()
+        self._setup_output()
 
-    def _setup_args(self):
+    def _setup_inputs(self):
         if not self.parser:
             return
 
@@ -24,6 +25,7 @@ class ArgParser:
 
         # directory flag argument, non positional
         group.add_argument(
+            "-d",
             "--dir",
             nargs="?",
             const=".",
@@ -31,11 +33,17 @@ class ArgParser:
             help="specify a directory to parse, defaults to the current one",
         )
 
+    def _setup_output(self):
+        if not self.parser:
+            return
+
         # optional output directory
-        group.add_argument(
+        self.parser.add_argument(
             "-o",
             "--output",
-            nargs="?",
             default=".",
             help="specify an output directory, defaults to the current one",
         )
+
+    def parse_args(self):
+        return self.parser.parse_args()
