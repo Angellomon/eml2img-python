61d988afbc4d68e9d4c47ef6d67ae8dd093c27b8
angellomon <angelmtzdiaz@gmail.com>
2025-09-01 16:20:32 -0600

    feat: main
    Implement the arg parser on the main script

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from args import ArgParser
+
+
 def main():
-    print("Hello from email2img!")
+    args_parser = ArgParser()
+    args = args_parser.parse_args()
+
+    filename: str = args.filename
+    output_dir: str = args.output
+    directory: str | None = args.dir
+
+    print(f"{filename=}, {output_dir=}, {directory=}")
 
 
 if __name__ == "__main__":
