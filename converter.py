545a5147087e466441bfc393670ff60c0415b517
angellomon <angelmtzdiaz@gmail.com>
2025-09-04 16:57:37 -0600

    fix: directories support & output
    + Fixed a bug that prevented direcotries form being converted
    + support for output directories

--- converter.py
+++ converter.py
@@ -4,30 +4,65 @@ from pathlib import Path
 
 
 class Eml2Img:
-    _filename: Path
-    _directory: Path | None
-    _output: Path
-
-    is_dir: bool
+    _filename: Path | None = None
+    _directory: Path | None = None
+    _output_base: Path
+    _output_dir: Path
+    _is_default_output: bool
 
     def __init__(
-        self, _filename: str, _directory: str | None = None, _output: str = "."
+        self, _filename: str | None, _directory: str | None = None, _output: str = "."
     ) -> None:
-        self._filename = Path(_filename)
-        self._directory = Path(_directory) if _directory else None
-        self._output = Path(_output)
+        if _filename is None:
+            if _directory is None:
+                raise ValueError("No filename or irectory where provided.")
 
-        self.is_dir = self._directory is not None
+            self._directory = self._resolve_path(_directory)
+        else:
+            self._filename = self._resolve_path(_filename)
 
-    def _remove_extension(self, file: io.TextIOWrapper, sep: str = ".") -> str:
+        self._output_base = self._resolve_path(_output)
+        self._output_dir = self._output_base
+        self._is_default_output = _output == "."
+        self._output_base.mkdir(exist_ok=True)
+
+    def _resolve_path(self, path: str) -> Path:
+        if path.startswith("~"):
+            return Path(path).expanduser()
+
+        if path.startswith("."):
+            return Path(path).resolve()
+
+        return Path(path)
+
+    @property
+    def is_dir(self) -> bool:
+        return self._filename is None and self._directory is not None
+
+    def _remove_path_extension(self, file: io.TextIOWrapper, sep: str = ".") -> str:
         return file.name.split(sep)[0]
 
-    def _parse_file(self, file: io.TextIOWrapper):
-        if not self._output.name:
-            name = self._remove_extension(file)
-            self._output = self._output.joinpath(name)
+    def _remove_file_extension(self, filename: str, sep: str = ".") -> str:
+        return filename.split(sep)[0]
 
-        self._output.mkdir(exist_ok=True)
+    def _get_last_part(self, file: io.TextIOWrapper) -> str:
+        return Path(file.name).parts[-1]
+
+    def _parse_file(self, file: io.TextIOWrapper):
+        filename = self._get_last_part(file)
+        filepath = self._remove_file_extension(filename)
+
+        # if not self._output_base.name:
+        #     self._output_dir = self._output_base.joinpath(filepath)
+        if self.is_dir and self._is_default_output:
+            assert self._directory is not None
+            self._output_dir = self._directory.joinpath(filepath)
+        # else:
+        #     self._output_dir = self._output_base
+        else:
+            self._output_dir = self._output_base.joinpath(filepath)
+
+        self._output_dir.mkdir(exist_ok=True)
 
         boundary = ""
         has_found_image = False
@@ -35,14 +70,10 @@ class Eml2Img:
         image_name = ""
         image_content = ""
 
-        CONTENT_TYPE_IMG_JPEG = "Content-Type: image/jpeg;"
-        CONTENT_TYPE_IMG_JPG = "Content-Type: image/jpg;"
-        CONTENT_TYPE_IMG_PNG = "Content-Type: image/png;"
+        image_types = ["jpeg", "jpg", "png"]
 
         CONTENT_TYPES_IMG = [
-            CONTENT_TYPE_IMG_JPEG,
-            CONTENT_TYPE_IMG_JPG,
-            CONTENT_TYPE_IMG_PNG,
+            f"Content-Type: image/{img_type};" for img_type in image_types
         ]
 
         CONTENT_TYPE_MULTIPART = "Content-Type: multipart/related;"
@@ -94,24 +125,24 @@ class Eml2Img:
         if self._directory is None:
             return
 
-        for file in self._directory.glob("**/*.eml"):
+        for file in self._directory.glob(r"**/*.eml"):
             with file.open("r") as _file:
                 self._parse_file(_file)
 
     def _save_image(self, filename: str, content: str):
         try:
-            path = self._output.joinpath(filename)
+            path = self._output_dir.joinpath(filename)
 
             with path.open("wb") as img_file:
                 img_file.write(base64.b64decode(content))
-        except Exception:
+        except Exception as e:
+            print(e)
             return
 
     def parse_eml(self):
-        if self.is_dir:
+        if self.is_dir or self._filename is None:
             self._parse_directory()
 
             return
-
         with self._filename.open("r") as file:
             self._parse_file(file)
